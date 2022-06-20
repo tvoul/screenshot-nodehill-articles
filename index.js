78078460a50166d49db4b375dd7596d2a9f14355
tvoul <t.voulgaris@hotmail.com>
2022-06-20 11:00:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ const { remote } = require('webdriverio');
         //find the last backslash
         let lastSlash = links[i].lastIndexOf('/')
         //cut out the title after the last backslash (otherwise path is invalid)
+        //i added to title so they are stored in order, newest to oldest
         let title = 'screenshots/' + i + '-' + links[i].slice(lastSlash+1) + '.png'
 
         //take a screenshot - saves as title
